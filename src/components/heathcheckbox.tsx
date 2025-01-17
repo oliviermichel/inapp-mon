ccf4ebfaae7c353b0cef580de6de19f9c5a97384
Olivier Michel <olivier.michel@gmail.com>
2025-01-17 13:25:26 +0100

    .

--- src/components/heathcheckbox.tsx
+++ src/components/heathcheckbox.tsx
@@ -11,17 +11,19 @@ interface HealthCheckBoxProps {
 const HealthCheckBox: React.FC<HealthCheckBoxProps> = ({ Title, isCheck }) => {
     const cardStyle = {
         border: "1px solid #ddd",
-        padding: "10px",
-        margin: "10px",
+        padding: "20px",
+        margin: "20px",
         boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
         display: "flex",
         justifyContent: "center",
-        alignItems: "center"
+        alignItems: "center",
+        width: "50%" // Set the width to 50% of the containing container
     };
 
     return (
         <div style={cardStyle}>
-            {Title}: {isCheck ? <FaCheckCircle  size={30} color='green' /> : <RxCrossCircled size={30} color='red' />}
+            <span style={{ fontSize: "1.5rem", marginRight: "10px" }}>{Title}:</span>
+            {isCheck ? <FaCheckCircle size={40} color='green' /> : <RxCrossCircled size={40} color='red' />}
         </div>
     );
 };
